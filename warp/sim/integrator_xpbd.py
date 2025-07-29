54538728af840fbbc76e707bdbc14bed40bc7c71
Gabriel Johnson <gjohns57@vols.utk.edu>
2025-07-29 12:25:23 -0400

    trying to add material properties to solve_tetrahedra

--- warp/sim/integrator_xpbd.py
+++ warp/sim/integrator_xpbd.py
@@ -575,8 +575,8 @@ def solve_tetrahedra(
 
     # act = activation[tid]
 
-    # k_mu = materials[tid, 0]
-    # k_lambda = materials[tid, 1]
+    k_mu = materials[tid, 0]
+    k_lambda = materials[tid, 1]
     # k_damp = materials[tid, 2]
 
     x0 = x[i]
@@ -629,7 +629,7 @@ def solve_tetrahedra(
             compliance = stretching_compliance
         elif term == 1:
             # volume conservation
-            C = wp.determinant(F) - 1.0
+            C = wp.determinant(F) - 1.0 + k_mu / k_lambda
             dC = wp.matrix_from_cols(wp.cross(f2, f3), wp.cross(f3, f1), wp.cross(f1, f2))
             compliance = volume_compliance
 
@@ -3008,7 +3008,7 @@ class XPBDIntegrator(Integrator):
                         # tetrahedral FEM
                         if model.tet_count:
                             wp.launch(
-                                kernel=solve_tetrahedra2,
+                                kernel=solve_tetrahedra,
                                 dim=model.tet_count,
                                 inputs=[
                                     particle_q,
